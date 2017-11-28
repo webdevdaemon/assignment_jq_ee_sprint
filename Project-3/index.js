52d5444843800c6d71a948eda9846e5c6dffc0d8
webDevDaemon - (Charles M.) <webdevdaemon@gmail.com>
2017-11-27 19:16:14 -0800

    refactored game functions, made more spaghetti

--- Project-3/index.js
+++ Project-3/index.js
@@ -1,28 +1,38 @@
 const INIT_STATE = {
-  on: false,
-  user_turn: false,
-  strict: false,
-  error: false,
-  sequence: [],
-  segment: [],
-  clicked: [],
-  input: '',
-  length: 0,
-};
+    on: false,
+    running: false,
+    user_turn: false,
+    strict: false,
+    error: false,
+    sequence: [],
+    segment: [],
+    clicked: [],
+    input: '',
+    length: 0,
+  },
+  _getNewState = (options = {}) => new GameState(options);
 
-function GameStateInstance(config = {}) {
-  let o = Object.assign({}, INIT_STATE, config), output = {};
-  for (let x in o) { this[x] = o[x]; }
+function GameState(mods = {}, base = INIT_STATE) {
+  let o = Object.assign({}, base, mods);
+  for (let x in o) {
+    this[x] = o[x];
+  }
 }
 
-let __st = new GameStateInstance(), st__ = {};
+let _now_ = new GameState(),
+  _previous_ = {},
+  _history_ = [],
+  _updater_ = {};
 
 $(document).ready(function() {
   $(document).on('click', 'div, span', function(e) {
     e.preventDefault();
     e.stopPropagation();
-    console.log({now});
+    afterEveryClick(e);
   });
+  //
+  // ─── JQUERY OBJECTS ─────────────────────────────────────────────────────────────
+  //
   const $power_switch = $('.switch'), // <-- $ Power Switch
     $power_knob = $('.knob'), // <-- $ Power Knob
     $color_panels = $('.panel'), // <-- $ Color Panels
@@ -30,129 +40,182 @@ $(document).ready(function() {
     $strict_mode = $('.strict'), // <-- $ Strict Mode Button
     $length_display = $('.digits'), // <-- $ Length Display
     $game_status = $('.status'); // <-- $ Status Display
-  $length_display.text('--');
-  $(document).on('click', 'div.panel', function(e) {
-    afterEveryClick(e);
-  });
 
-  const _getNewState = (mods_obj = {}) => (
-    new GameStateInstance(config, x, x)
-  );
-  let fn = {
-    updateUI(curr = now, prev = the_previous) {},
-    cycleUpdate(nu_state = {}) {
-      the_previous = now;
-      now = nu_state;
-      updateUI(now);
-    },
-    afterEveryClick(event) {
-      let t = event.currentTarget;
-      if (now.on && now.user_turn && t.id.length === 1) {
-        cycleUpdate({
-          input: t.id,
-          clicked: [...now.clicked, t.id],
-        });
-        return;
+  function afterEveryClick(e) {
+    let id = e.currentTarget.id;
+    updateState(
+      _now_.on && id.length === 1 ?
+      {
+        input: id,
+        clicked: [..._now_.clicked, id],
+      } :
+      {
+        input: '--',
+      },
+    );
+    console.log({
+      _now_,
+      _updater_,
+      _history_
+    });
+    console.table([_now_, _previous_]); // <- DEBUG
+  }
+
+  function updateUI(state = _now_) {}
+
+  function updateState(updates = {}) {
+    _previous_ = _getNewState(_now_);
+    _updater_ = updates;
+    _history_ = [_getNewState(_now_), ..._history_];
+    _now_ = _getNewState(updates);
+  }
+
+  function updateLengthDisplay(length = 0) {
+    $length_display.text(length !== null ? _getDisplayLength(length) : '');
+  }
+
+  function _getNewSequence(length = 20) {
+    let colors = ['r', 'g', 'b', 'y'],
+      seq = [],
+      i;
+    for (i = 0; i < length; i++) {
+      let n = Math.floor(Math.random() * 3.999);
+      seq = [...seq, colors[n]];
+    }
+    return seq;
+  }
+
+  function _getDisplayLength(num = 0) {
+    num > 9 ? `${num}` : `0${num}`;
+  }
+
+  function updateSegment(length = 0) {
+    let segment = _now_.sequence.slice(0, _now_.segment.length);
+    updateState({
+      segment,
+      length
+    });
+  }
+
+  function playColorPanelAudio(str) {
+    $(`#panel-tone-${str}`)
+      .get(0)
+      .play();
+  }
+
+  function activatePanel(color_str) {
+    let $panel = $(`#${color_str}`), ms = _now_.error ? 2000 : 750;
+    if (!_now_.error) {playColorPanelAudio(color_str);}
+    $panel.addClass('active');
+    setTimeout(function() { $panel.removeClass('active'); }, ms);
+  }
+
+
+  function playbackSequence(l = _now_.length) {
+    let i = 0;
+    updateLengthDisplay(i);
+    playbackSequence.playbackInterval = setInterval(function() {
+      if (i < l) {
+        activatePanel(_now_.sequence[i]);
+        i++;
+        updateLengthDisplay(i);
+      } else {
+        updateState({user_turn: true});
+        clearInterval(playbackSequence.playbackInterval);
       }
-    },
-    newSequence(length = 20) {
-      let colors = ['r', 'g', 'b', 'y'],
-        seq = [],
-        i;
-      for (i = 0; i < length; i++) {
-        let n = Math.floor(Math.random() * 3.999);
-        seq = [...seq, colors[n]];
-      }
-      console.log(seq);
-      return seq;
-    },
-    updateSegment(len = 0) {
-      let segment = now.sequence.slice(0, now.segment.length);
-      cycleUpdate({
-        segment,
-        length: len,
-      });
-    },
-    activatePanel(color_str) {
-      let $panel = $(`#${color_str}`),
-        ms = now.error ? 2000 : 750;
-      $panel.addClass('active');
-      setTimeout(function() {
-        $panel.removeClass('active');
-      }, ms);
-    },
-    checkUserInput(user_input = '') {
-      if (user_input === now.sequence[length + 1]) {
-        now = {
-          length: now.length ? now.length - 1 : 0,
-        };
-        return true;
-      }
-      return false;
-    },
-    displayLength(num = 0) {
-      let str = num > 9 ? `${num}` : `0${num}`;
-      $length_display.text(str);
-    },
-    playbackSequence(l = now.length) {
-      let i = 0;
-      displayLength(i);
-      now.playbackInterval = setInterval(function() {
-        if (i < l) {
-          activatePanel(now.sequence[i]);
-          i++;
-          displayLength(i);
-        } else {
-          now.user_turn = !now.user_turn;
-          clearInterval(now.playbackInterval);
-        }
-      }, 850);
-    },
-    errorSequence() {
-      let $not_red = $color_panels.not('.red');
-      console.log($not_red); // <-- test
-      now.error = true;
-      $not_red.addClass('error');
-      ['r', 'g', 'b', 'y'].forEach(activatePanel);
+    }, 780);
+  }
+
+  function errorSequence(muted = false) {
+    let $not_red = $color_panels.not('.red');
+    if (!muted) {
       $('#no')
-        .get(0)
-        .play();
-      $length_display.text('Er');
-      setTimeout(function() {
-        $not_red.removeClass('error');
-        displayLength(now.length);
-      }, 3000);
-      now.error = false;
+      .get(0)
+      .play();
+    }
+    $length_display.text(muted ? ':)' : 'XX');
+    updateState({
+      error: true,
+    });
+    $not_red.addClass('error');
+    ['r', 'g', 'b', 'y'].forEach(activatePanel);
+    setTimeout(function() {
+      $not_red.removeClass('error');
+      updateLengthDisplay(_now_.length);
+    }, 3000);
+    updateState({
+      error: false,
+    });
+  }
+
+  function checkUserInput(user_input = '') {
+    if (user_input === _now_.sequence[length - 1]) {
+      updateState({
+        length: _now_.length ? _now_.length - 1 : 0,
+      });
+      return true;
+    }
+    return false;
+  }
+
+  let game = {
+    init(on = _now_.on) {
+      if (on) {
+        errorSequence(true);
+        updateState(
+          new GameState({
+            on: true,
+            user_turn: false,
+            sequence: _getNewSequence(20),
+            length: 5
+          })
+        );
+        updateState({segment: _now_.sequence.slice(_now_.length)});
+      } else {
+        updateState(new GameState());
+        updateLengthDisplay(null);
+      }
     },
+    startReset() {
+
+    }
   };
+
+  // POWER SWITCH CLICK HANDLER
   $power_switch.click(function() {
-    $power_knob.text(now.on ? '' : 'ON');
+    updateState({on: !_now_.on});
+    let onn = _now_.on;
+    $length_display.text(onn ? '--' : '');
+    $power_knob.text(onn ? 'ON' : '');
     $power_switch.toggleClass('on');
     $length_display.toggleClass('on');
-    now.on = !now.on;
-    if (now.on) {
-      now.sequence = newSequence(5);
-      console.log(now);
-      playbackSequence(now.length);
-      errorSequence();
-    }
+    game.init(onn);
   });
   // COLOR PANELS CLICK HANDLER
   $color_panels.click(function() {
     let id = $(this).get(0).id,
-      ok = checkUserInput(id, now.length);
+      ok = checkUserInput(id, _now_.length);
     activatePanel(id);
-    if (ok) {
-      displayLength(now.length + 1);
-
+    if (_now_.running && ok) {
+      updateState({length: _now_.length + 1});
+      updateLengthDisplay(_now_.length);
       console.log('O%', $(this).get(0).id, 'CORRECT!!!');
-    } else {
-    }
-    now.input = id;
+    } else {}
+    updateState({
+      input: id
+    });
   });
-  $start_reset.on('click', function() {});
+  // START-RESET CLICK HANDLER
+  $start_reset.on('click', function() {
+    if (!_now_.on) {return;}
+    else if (_now_.running) {
+      game.init();
+      playbackSequence();
 
-  let game = {
-    newGame() {},
-  };
+    }
+  });
 });
+
+// game on
+
+// 1
